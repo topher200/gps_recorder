01df062bc4b5e54091e73b497d19e04f697af0c4
Topher Brown <topher200@gmail.com>
2011-06-12 03:31:01 -0400

    fixup for toast messages

--- gps_recorder.py
+++ gps_recorder.py
@@ -24,25 +24,26 @@ def record():
   droid.makeToast("Starting location drill")
 
   # Show a dialog box
-  droid.dialogCreateInput("GPS Recorder running")
+  droid.dialogCreateInput("GPS Recorder running", "Add message to log")
   droid.dialogSetPositiveButtonText("Log message")
   droid.dialogSetNegativeButtonText("Exit")
   droid.dialogShow()
 
   # Loop until the user exits
-  with open(determine_filename(), 'w') as out_file:
+  out_filename = determine_filename()
+  with open(out_filename, 'w') as out_file:
     running = True
     while running:
       res = droid.eventWait(1000).result
       if res == None:
         print "LocationListener timeout"
       elif res['name'] == "dialog":
-        print "Dialog pressed"
         if (res[u'data'][u'which'] == u'positive'):
-          # log the message
+          droid.makeToast("Saving log message")
           message = '# {0}'.format(res[u'data'][u'value'])
           print message
-          out_file.write(message)
+          out_file.write('{0}\n'.format(message))
+          droid.dialogShow()
         else:
           print "User requested exit"
           running = False
@@ -57,9 +58,8 @@ def record():
 
     # Shutdown
     droid.stopLocating()
-    droid.makeToast("Recording done, saving")
 
-  droid.makeToast("All done - exiting!")
+  droid.makeToast("All done - saved to {0}".format(out_filename))
 
 if __name__ == '__main__':
   record()
