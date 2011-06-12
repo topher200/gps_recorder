ae23e661189cdece930680434e79e97189886c30
Topher Brown <topher200@gmail.com>
2011-06-11 22:54:29 -0400

    gps_recorder: stopping on user command

--- gps_recorder.py
+++ gps_recorder.py
@@ -6,17 +6,31 @@ import time
 def record(seconds = 10):
   droid = android.Android()
   droid.startLocating(0, 0)
-  # TODO(topher): will need this when not debugging
-  # time.sleep(1)
   droid.makeToast("Starting location drill")
+  droid.dialogCreateAlert("Exit?")
+  droid.dialogSetNeutralButtonText("Exit")
+  droid.dialogShow()
   results = []
-  start = time.time()
-  while((time.time() - start) < seconds):
-    res = droid.eventWait(1000)
-    try:
-      loc = res.result['data']['gps']
-    except (KeyError, TypeError):
-      print("Error: no gps found")
+  running = True
+  while running:
+    res = droid.eventWait(1000).result
+    if res == None:
+      print "LocationListener timeout"
+      continue
+    elif res['name'] == "dialog":
+      # Dialog has been pressed- get out
+      print "User requested exit"
+      running = False
+      continue
+    elif res['name'] == "location":
+      try:
+        loc = res['data']['gps']
+      except (KeyError, TypeError):
+        print("Location message, but no GPS data")
+        continue
+    else:
+      print "I have no idea what kind of message that was"
+      print res
       continue
     print(loc)
     results.append(loc)
