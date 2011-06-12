867b8b3d01369f7f675db87d8028b284d3b357fe
Topher Brown <topher200@gmail.com>
2011-06-11 23:26:24 -0400

    gps_recorder: using with_statement

--- gps_recorder.py
+++ gps_recorder.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.6
+from __future__ import with_statement
 import android
 import json
 import time
@@ -10,38 +11,32 @@ def record():
   droid.dialogCreateAlert("GPS Recorder running")
   droid.dialogSetNeutralButtonText("Exit")
   droid.dialogShow()
-  results = []
-  running = True
-  while running:
-    res = droid.eventWait(1000).result
-    if res == None:
-      print "LocationListener timeout"
-      continue
-    elif res['name'] == "dialog":
-      # Dialog has been pressed- get out
-      print "User requested exit"
-      running = False
-      continue
-    elif res['name'] == "location":
-      try:
-        loc = res['data']['gps']
-      except (KeyError, TypeError):
-        print("Location message, but no GPS data")
+  with open('gps_output.txt', 'w') as out_file:
+    running = True
+    while running:
+      res = droid.eventWait(1000).result
+      if res == None:
+        print "LocationListener timeout"
         continue
-    else:
-      print "I have no idea what kind of message that was"
-      print res
-      continue
-    print(loc)
-    results.append(loc)
-  droid.stopLocating()
-  droid.makeToast("Recording done, saving")
-
-  try:
-    f = open('gps_output.txt', 'w')
-    json.dump(results, f)
-  finally:
-    f.close()
+      elif res['name'] == "dialog":
+        # Dialog has been pressed- get out
+        print "User requested exit"
+        running = False
+        continue
+      elif res['name'] == "location":
+        try:
+          loc = res['data']['gps']
+        except (KeyError, TypeError):
+          print("Location message, but no GPS data")
+          continue
+      else:
+        print "I have no idea what kind of message that was"
+        print res
+        continue
+      print(loc)
+      out_file.write(json.dumps(loc) + '\n')
+    droid.stopLocating()
+    droid.makeToast("Recording done, saving")
 
   droid.makeToast("All done - exiting!")
 
